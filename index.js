5b26855618e005cd3d59fa5d02b98c5a0df6dc18
Bet4 <0xbet4@gmail.com>
2019-02-14 09:51:59 +0800

    Add basic support for restoring layout from YAML config

--- index.js
+++ index.js
@@ -1,60 +1,38 @@
+const fs = require('fs')
+const yaml = require('js-yaml')
+const untildify = require('untildify')
+
 let hyperlayout
 
-// Resolves an array, to find deepest cild. [[[1]]] -> 1
-const resolveArray = a => a instanceof Array ? resolveArray(a[0]) : a
-
-// Walk through modes – WINDOW -> TAB -> HORIZONTAL -> VERTICAL -> HORIZONTAL -> ...
-const nextMode = mode => {
-  switch (mode) {
-    case 'TAB':
-    case 'VERTICAL':
-      return 'HORIZONTAL'
-    case 'PANE':
-    case 'HORIZONTAL':
-      return 'VERTICAL'
-    case 'WINDOW':
-      return 'TAB'
-    default:
-      return 'WINDOW'
-  }
-}
-
-// Generate Command queue from converted Config
-function generateQueue(converted, mode = 'TAB', initial) {
-  mode = (mode === 'PANE') ? 'HORIZONTAL' : mode
-
+// Generate Command queue from converted layout tree
+function generateQueue(converted, initial) {
   let q = []
 
-  if (converted instanceof Array) {
-    converted.forEach((item, i) => {
+  if (converted.cells instanceof Array) {
+    converted.cells.forEach((cell, i) => {
       if (i > 0) {
         q.push({
           action: 'split',
-          mode,
-          pane: resolveArray(item)
+          mode: converted.type,
+          pane: {index: cell.id}
         })
       } else {
         q.push({
           action: 'jump',
-          pane: resolveArray(item)
+          pane: {index: cell.id}
         })
       }
       if (initial || i > 0) {
         q.push({
           action: 'cmd',
-          pane: resolveArray(item)
+          pane: {index: cell.id}
         })
       }
     })
-    converted.forEach(item => {
-      q = q.concat(generateQueue(item, nextMode(mode)))
+    converted.cells.forEach(cell => {
+      q = q.concat(generateQueue(cell))
     })
   }
-  // Jump back to initial pane
-  q.push({
-    action: 'jump',
-    pane: resolveArray(converted)
-  })
   return q
 }
 
@@ -64,12 +42,16 @@ class Hyperlayout {
     this.cwd = cwd
     this.store = store
     this.panes = []
+    this.paneNum = 0
     this.lastIndex = 0
     this.knownUids = []
+    this.layoutTree = {}
 
-    const converted = this.convertConfig(config.layout)
-    const entry = (config.entry || 'tab').toUpperCase()
-    this.queue = generateQueue(converted, entry, true)
+    const layoutPtr = {s: config.layout.substr(5)}
+    this.layoutTree = this.layoutConstruct(null, layoutPtr)
+    Array.from(new Array(this.paneNum), (x, i) =>
+               this.panes.push({index: i, cmd: config.panes[i]}))
+    this.queue = generateQueue(this.layoutTree, true)
     this.work()
   }
   work() {
@@ -105,17 +87,83 @@ class Hyperlayout {
           this.work()
         }
       }
+    } else {
+      this.layoutResize(this.layoutTree,
+                        this.store.getState().termGroups.activeRootGroup)
     }
   }
-  convertConfig(item) {
-    if (item instanceof Array) {
-      return item.map(this.convertConfig.bind(this))
-    } else if (typeof item === 'string') {
-      const pane = {cmd: item, index: this.panes.length}
-      this.panes.push(pane)
-      return pane
+  // A javascript rewrite of layout_construct in tmux
+  layoutConstruct(lcParent, layoutPtr) {
+    const posPattern = /^(\d+)x(\d+),(\d+),(\d+)(.*)$/
+    const wpidPattern = /^,\d+([^x].*)$/
+    const pos = posPattern.exec(layoutPtr.s)
+    const lc = {}
+    lc.parent = lcParent
+    lc.sx = parseInt(pos[1], 10)
+    lc.sy = parseInt(pos[2], 10)
+    lc.xoff = parseInt(pos[3], 10)
+    lc.yoff = parseInt(pos[4], 10)
+    lc.cells = []
+    lc.id = this.paneNum
+    layoutPtr.s = pos[5]
+    const wpid = wpidPattern.exec(layoutPtr.s)
+    if (wpid) {
+      layoutPtr.s = wpid[1]
     }
-    console.error('Wrong type:', item)
+    switch (layoutPtr.s[0]) {
+      case ',':
+      case '}':
+      case ']':
+      case undefined: {
+        this.paneNum = this.paneNum + 1
+        return lc
+      }
+      case '{':
+        lc.type = 'LAYOUT_LEFTRIGHT'
+        break
+      case '[':
+        lc.type = 'LAYOUT_TOPBOTTOM'
+        break
+      default:
+    }
+    do {
+      layoutPtr.s = layoutPtr.s.substr(1)
+      lc.cells.push(this.layoutConstruct(lc, layoutPtr))
+    } while (layoutPtr.s[0] === ',')
+
+    switch (lc.type) {
+      case 'LAYOUT_LEFTRIGHT':
+        if (layoutPtr.s[0] !== '}') {
+          console.error('Layout format wrong!')
+        }
+        break
+      case 'LAYOUT_TOPBOTTOM':
+        if (layoutPtr.s[0] !== ']') {
+          console.error('Layout format wrong!')
+        }
+        break
+      default:
+        console.error('Layout format wrong!')
+        break
+    }
+    layoutPtr.s = layoutPtr.s.substr(1)
+    return lc
+  }
+  layoutResize(layoutTree, termGroupUid) {
+    const termGroups = this.store.getState().termGroups.termGroups
+    const termGroupTree = termGroups[termGroupUid]
+    if (layoutTree.cells.length > 0 &&
+       layoutTree.cells.length === termGroupTree.children.length) {
+      let sizes
+      if (layoutTree.type === 'LAYOUT_LEFTRIGHT') {
+        sizes = layoutTree.cells.map(cell => cell.sx / layoutTree.sx)
+      } else if (layoutTree.type === 'LAYOUT_TOPBOTTOM') {
+        sizes = layoutTree.cells.map(cell => cell.sy / layoutTree.sy)
+      }
+      termgroupResize(this.store, termGroupUid, sizes)
+      layoutTree.cells.forEach((c, i) =>
+                               this.layoutResize(c, termGroupTree.children[i]))
+    }
   }
 }
 
@@ -123,9 +171,11 @@ class Hyperlayout {
 function requestSession(cwd, mode) {
   const payload = {cwd}
   switch (mode) {
-    case 'HORIZONTAL':
-    case 'VERTICAL':
-      payload.splitDirection = mode
+    case 'LAYOUT_LEFTRIGHT':
+      payload.splitDirection = 'VERTICAL'
+      break
+    case 'LAYOUT_TOPBOTTOM':
+      payload.splitDirection = 'HORIZONTAL'
       break
     default:
       break
@@ -151,6 +201,15 @@ function focusUid({dispatch}, uid) {
   })
 }
 
+// Resize TermGroup with given `uid` and `sizes`
+function termgroupResize({dispatch}, uid, sizes) {
+  dispatch({
+    type: 'TERM_GROUP_RESIZE',
+    uid,
+    sizes
+  })
+}
+
 // Listens for cli commands and sessions
 exports.middleware = store => next => action => {
   const {type, data, uid} = action
@@ -159,8 +218,11 @@ exports.middleware = store => next => action => {
   if (type === 'SESSION_ADD_DATA') {
     const testedData = /^\[hyperlayout config]:(.*)/.exec(data)
     if (testedData && testedData[1]) {
-      const config = JSON.parse(testedData[1])
-      hyperlayout = new Hyperlayout(config, store)
+      const cfg = yaml.safeLoad(fs.readFileSync(testedData[1], 'utf8'))
+      const cwd = untildify(cfg.root)
+      const windows = cfg.windows[0]
+      const config = windows[Object.keys(windows)[0]]
+      hyperlayout = new Hyperlayout({cwd, config}, store)
       return
     }
   }
